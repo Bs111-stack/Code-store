57dac4dee4e6268159abb2a558b44b5959cbd0f8
Bs111-stack <nirmalumra005@gmail.com>
2025-10-17 13:06:19 +0530

    var and data type
    in this i have introduced about var and data types..

---
+++ var and data.py
@@ -0,0 +1,26 @@
+""" print("hello") """
+a=123
+print(a)
+""" a9="harry"
+print(a+a9) """
+b1=4
+print(a,b1)
+print("the type of a is",type(a))#1.1 is float...
+h=complex(1,7)
+print(h)
+p=complex(12,567)#this is a complex number...
+print(p) 
+#list is a collection of different data types or elements..
+list23=[8,9,90,[-3,908],["apple","banana","orange"]]
+print(list23)
+#tuple are immutable,means we cant change the value of tuple... 
+tuple1=(("parrot","hello","my"))
+print(tuple1)#list in tuple can change..but if i change this and write tuple1=("grapes")..shows error...
+#how to add new tuple in tuple..m
+mytuple=("app","an","das")
+newtuple=mytuple+("android",)#adding a new tuple in mytuple...(use the comma(,))
+print(newtuple)
+#dictionery is a map data..{}use curely bracets for dictionery...
+dict4={"name":"pie","age":1,"canvote":"no"}
+print(dict4)# use ratio(:)in dictionery,compelting one one use comma(,)...
+#in python everthing is a object
