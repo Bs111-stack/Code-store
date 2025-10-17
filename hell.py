c63df8dfa7631959b463e9d284300ecf3d0bc523
Bs111-stack <nirmalumra005@gmail.com>
2025-10-17 11:57:09 +0530

    sep and end
    in python we use sep means seperate and end,sep is used when we want to join the characters with each other and end is used when we want to add more something in previous one..

---
+++ hell.py
@@ -0,0 +1,26 @@
+#num1=float(input("enter the 1st num"))
+#num2=float(input("enter the 2nd num"))
+#print(num1 + num2)
+#ranges
+#for i in range(8):
+   # print("hell")g
+#print(17*7)
+'''
+print("hello" \
+"worlds")
+'''
+print("hell")
+"""
+print("hello" \
+"worlds")
+""" #used in comments,means hide no error occurs
+# multi line comment shortcut key shift+alt+A   
+print("hey",1,2,3,4)
+print("hell",1,2,3,sep="#",end="\n")#if you want to join the text then use this~ like this see the print...sep=seperate,its a operator.
+print("python")#use end="",we use this in previou print ^^ to add more value to previous output example....(,end="009").
+#want that python display seperatelly use\n example..
+def add(a, b):
+    """This function adds two numbers and returns the result."""
+    return a + b
+
+print(add.__doc__)
