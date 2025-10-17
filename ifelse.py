1dc9d7bfc8449139300558c5cc61ae2e822ee0aa
Bs111-stack <nirmalumra005@gmail.com>
2025-10-17 12:00:13 +0530

    use of if else and operators.
    in this if-elif-else and operators are used even i have introduced all the operators and also set the examples of each operator..

---
+++ ifelse.py
@@ -0,0 +1,63 @@
+'''conditional operators..
+     >,<,>=,<=,==,!=(not equal to),
+    '''
+#example of each conditional opeator..
+'''
+print(a>18)
+print(a<18)
+print(a>=18)
+print(a<=18)
+print(a==18)
+print(a!=18)
+'''
+#if else condotional...(if,else if,else)..
+'''
+a =int(input("enter your age"))
+print("your age is:",a)
+if(a>18):
+    print("you can drive")
+
+    print("")
+else:
+    print("you can not drive,get license.")
+    '''
+#elif use..
+"""
+appleprice = 10
+budget = 10
+
+if budget > appleprice:
+    print("yes can can buy,and can add more")
+elif appleprice - budget < 50:
+    print("YOU can  buy, but can not add more")
+else:
+    print("go on another shop")
+    """
+num = int(input("enter the number"))
+if(num==0):
+    print("the number is zero")
+elif(num<0):
+    print("the number is negative")
+else:
+    print("the number is positive")
+    '''
+# Define the numbers
+a = 12
+b = 12
+
+# Addition
+addition = a + b
+print("Addition:", addition)
+
+# Subtraction
+subtraction = a - b
+print("Subtraction:", subtraction)
+
+# Multiplication
+multiplication = a * b
+print("Multiplication:", multiplication)
+
+# Division
+division = a / b
+print("Division:", division)
+'''
