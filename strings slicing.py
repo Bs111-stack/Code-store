2b7a6f32c209f75cb82ae06ad4096733e2713f2b
Bs111-stack <nirmalumra005@gmail.com>
2025-10-17 12:17:58 +0530

    how strings slicing works
    in this i explained you how the string slicing works , explanation in my own language..

---
+++ strings slicing.py
@@ -0,0 +1,14 @@
+#print("hello")
+#use string with more advance..
+names=("jaiopo,yash,aruo")
+print(names[8:13])#in this 8 defines ash and 13 defines a,means ouptput will be ash,a..
+#in this 0=j and 1 will not be taken it means 0:1 defines from start to end text...
+#if you want the length of a character...use this len(names)..
+print(len(names))
+#my=("hello","my")
+#print(my)
+jklon="bhupendra"
+print(jklon[4:len(jklon)-3]) #-3 defines the len used in python,python wiill auto matically take it..len of text -(-3)...
+for i in range(5):# defines how many number of times you want to display a character..
+    print("range")# range is the name you can ad more names in it..
+print(len("hello"))
