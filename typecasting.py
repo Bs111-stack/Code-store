5ab7dcdfb6eae0cfeda988654b9a8b06568e678b
Bs111-stack <nirmalumra005@gmail.com>
2025-10-17 13:04:41 +0530

    explicit and implicit
    in this i have given a example of explicit and the implicit..

---
+++ typecasting.py
@@ -0,0 +1,9 @@
+print("hello")
+#explicit typecasting...string to interger
+a="6"
+b="4"
+print(int(a)+int(b))
+#implicit typecasting....Python automatically converts a (integer) to a float before performing the addition.
+c=1.9
+d=5
+print(c+d)
