211d1fa398cfbf5b6461d829717fb67038823289
Bs111-stack <nirmalumra005@gmail.com>
2025-10-17 12:38:20 +0530

    strings info
    in this i just introduced you some strings,which is useful.

---
+++ strings.py
@@ -0,0 +1,25 @@
+""" a="12"
+b="12"
+print(a+b)
+ """
+#strings start...
+#string are two types double quote"" and single quote''
+name="ro"
+friend='juo'
+#print("hello"+name)
+#sapple="hi i am a good boy,\"and you."#cant use string into string,if problem occures use back slash.
+#print(sapple)
+#Multi Lines string...(''' ''')
+quat=''' he said,
+he is not a good boy
+and aslo he is a cheater
+'''
+print(quat)
+#indexing...
+my="python"
+print(my[1])
+#print("the type of my is",type(my))
+print("lets use for loop,not that loop\n")
+for character in my:# used to print the each letters of a text after in this name will will be writen of a  identifier..
+    print(character)
+    
