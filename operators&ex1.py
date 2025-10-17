b56c16fe5d1b384087b256c49bad22b79bf87d34
Bs111-stack <nirmalumra005@gmail.com>
2025-10-17 13:00:21 +0530

    operators and excercise 1
    in this i have a excercise 1 where and operatoors uses are showed and you have to make a program that use all the operators and gives the output , take two numbers randomly..

---
+++ operators&ex1.py
@@ -0,0 +1,26 @@
+#print("hello")
+""" operators """
+print(12-10)
+print(12+10)
+print(12*10)
+print(12/10)
+print(12//10)#floor division operator....(gives the number only not in point like 1.1-->1)...
+print(5%3)#module operator...(gives remainder only)...
+print(12**2)#expontial it means 12 will multiply 2time 12*12...
+#task 1...
+o=14
+p=10
+ans1 = o+p
+print("addition of",o,"and",p,"is",ans1)
+ans2 = o-p
+print("subtraction of",o,"and",p,"is",ans2)
+ans3 = o*p
+print("multiply of",o,"and",p,"is",ans3)
+ans4 = o/p
+print("divide of",o,"and",p,"is",ans4)
+ans5 = o//p
+print("floor division of",o,"and",p,"is",ans5)
+ans6=o%p
+print("module of",o,"and",p,"is",ans6)
+ans7 = o**p
+print("expontial of",o,"and",p,"is",ans7)
