b4dce1be6f29f6f96e4da17f07b3e900b253fa03
Bs111-stack <nirmalumra005@gmail.com>
2025-10-17 12:06:57 +0530

    how to input your name
    in this i have introduced how to take input from user,also with a example.

---
+++ inputfromuser.py
@@ -0,0 +1,6 @@
+#print("hello")
+a=input("enter your name")#user will type his/her name
+print("my name is",a)#output comes
+x=input("enter the first number")
+y=input("enter the second number")
+print(int(x)+int(y))#int is used to change the value of string into a number so that then can add perfectly
